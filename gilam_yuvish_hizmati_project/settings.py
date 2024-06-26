60e67b6d3b3930131bef9f5d670f7bd114c31ece
1sulaymanov <ilyosbeksulaymanov2@gmail.com>
2024-06-26 17:27:55 +0500

    Fixing bugs

--- gilam_yuvish_hizmati_project/settings.py
+++ gilam_yuvish_hizmati_project/settings.py
@@ -63,16 +63,23 @@ WSGI_APPLICATION = 'gilam_yuvish_hizmati_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME':'gilam',
+#         'USER':'postgres',
+#         'PASSWORD':'isa',
+#         'HOST':'localhost',
+#         'PORT':'5432',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME':'gilam',
-        'USER':'postgres',
-        'PASSWORD':'isa',
-        'HOST':'localhost',
-        'PORT':'5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
