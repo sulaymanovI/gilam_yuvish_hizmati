1f6d1318864076c92c5d6222529e367fd4fe25de
1sulaymanov <ilyosbeksulaymanov2@gmail.com>
2024-06-26 17:29:27 +0500

    fixing bugs

--- gilam_yuvish_hizmati_project/settings.py
+++ gilam_yuvish_hizmati_project/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'gilam_yuvish_hizmati_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR  + '/' + 'db.sqlite3',
     }
 }
 
