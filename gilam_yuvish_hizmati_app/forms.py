7ac90d2ef68466f89b7d3284df94753be13b56e7
1sulaymanov <ilyosbeksulaymanov2@gmail.com>
2024-06-27 10:05:43 +0500

    fixing bugs

--- gilam_yuvish_hizmati_app/forms.py
+++ gilam_yuvish_hizmati_app/forms.py
@@ -6,3 +6,7 @@ class BuyurtmaForm(forms.ModelForm):
     class Meta:
         model=Buyurtma
         fields=['fullname','telefon_raqam','hizmat_turi','manzil']
+
+    def __init__(self,*args, **kwargs):
+        super(BuyurtmaForm , self).__init__(*args, **kwargs)
+        self.fields['manzil'].label="Manzil (Ixtiyoriy)" 
