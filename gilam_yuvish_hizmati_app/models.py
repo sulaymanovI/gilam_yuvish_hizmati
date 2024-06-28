0aaf66d0681bb068d5e92c59a005ae4a7ac42e64
1sulaymanov <ilyosbeksulaymanov2@gmail.com>
2024-06-28 16:07:21 +0500

    fixing bugs

--- gilam_yuvish_hizmati_app/models.py
+++ gilam_yuvish_hizmati_app/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.utils import timezone
 from django.core.validators import RegexValidator
+
 error_message_phone_validation="Noto'g'ri formatdagi telefon raqam kiritildi !"
 PHONE_REGEX=r"^\+?(?:\d{12}|\d{9})$"
+
 HIZMAT_TURLARI={
     "GY" : "Gilam yuvish",
     "YMY" : "Yumshoq mebel yuvish",
@@ -15,14 +17,12 @@ HIZMAT_HOLATI={
     "BD" : "Bajarildi",
 }
 
-
-
 class Buyurtma(models.Model):
     fullname=models.CharField(max_length=250,verbose_name="Ism Familiya")
-    telefon_raqam=models.CharField(max_length=13,validators=RegexValidator(
+    telefon_raqam=models.CharField(max_length=13,validators=[RegexValidator(
         regex=PHONE_REGEX,
         message=error_message_phone_validation
-    ),verbose_name="Telefon raqam")
+    )],verbose_name="Telefon raqam")
     hizmat_turi=models.CharField(max_length=125,choices=HIZMAT_TURLARI , default="Hizmat turini tanlang")
     hizmat_holati=models.CharField(max_length=125,choices=HIZMAT_HOLATI , default="Yangi")
     buyurtma_sanasi=models.DateTimeField(default=timezone.now, verbose_name="Buyurtma sanasi")
